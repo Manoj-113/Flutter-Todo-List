4eb3df8ec339b1c02782e56cf10a1ba24d6a7450
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-05-09 01:01:36 +0530

    Update main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ void main() =>
     
     class _MyAppState extends State<MyApp> {
       List todos = List();
+      String input = "";
 
 
       @override
@@ -34,6 +35,30 @@ void main() =>
         return Scaffold(
           appBar: AppBar(
             title: Text("mytodos"),
+          ),
+          floatingActionButton: FloatingActionButton(onPressed: 
+          (){
+            showDialog(context: context, 
+            builder:
+            (BuildContext context){
+              return AlertDialog(
+                title: Text("Add Todolist"),
+                content: TextField(
+                  onChanged: (String value){
+                    input = value;
+                  },
+                  actions: <Widget>[
+                    FlatButton(onPressed: (){
+                      setState(() {
+                        todos.add(input);
+                      });
+                    }, child: Text("Add"))
+                  ]
+                ),
+              );
+            });
+          },child: Icon(Icons.add,
+          color: Colors.white,),
           ),
           body: ListView.builder(
             itemCount: todos.length,
