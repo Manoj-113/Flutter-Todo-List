7d6a69415f7608d1a13e1364a5531be8c7169ed9
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-05-08 23:45:30 +0530

    Update the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -10,3 +10,41 @@ void main() =>
     home: MyApp(),
     ));
 
+    class MyApp extends StatefulWidget {
+      @override
+      _MyAppState createState() => _MyAppState();
+    }
+    
+    class _MyAppState extends State<MyApp> {
+
+
+      @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    todos.add("Item1");
+    todos.add("Item2");
+  }
+
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("mytodos"),
+          ),
+          body: ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (BuildContext context, int index){
+              return Dismissible(
+                key: Key(todos[index]), 
+                child: Card(
+                  child: ListTile(
+                    title: Text(todos[index]),
+                ),
+              ));
+            }),
+          
+        );
+      }
+    }
+
